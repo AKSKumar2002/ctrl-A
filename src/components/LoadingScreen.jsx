72d4afed5907ba70b47ead886ae9594d76264c16
Mr. AKS <aksnetlink@gmail.com>
2025-08-11 01:02:19 +0530

    2

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -4,7 +4,7 @@ import "./LoadingScreen.css";
 const LoadingScreen = ({ isFadingOut }) => {
   return (
     <div className={`loading-container ${isFadingOut ? "fade-out" : ""}`}>
-      <h1 className="loading-title">AKStack.dev</h1>
+      <h1 className="loading-title">ctrlA.labs</h1>
       <div className="spinner"></div>
     </div>
   );
